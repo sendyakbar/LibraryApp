c56ade1c4bfabe38e2c0618fb0b67709e91754d1
sendyakbar <msendyakbars@gmail.com>
2023-10-24 19:02:24 +0700

    chore: add icon assets

---
+++ src/themes/icons.ts
@@ -0,0 +1,10 @@
+export const icon = {
+  technology: require('../assets/icons/lib_technology.png'),
+  technologyInactive: require('../assets/icons/lib_technology_inactive.png'),
+  economics: require('../assets/icons/lib_economics.png'),
+  economicsInactive: require('../assets/icons/lib_economics_inactive.png'),
+  fictions: require('../assets/icons/lib_fictions.png'),
+  fictionsInactive: require('../assets/icons/lib_fictions_inactive.png'),
+  science: require('../assets/icons/lib_science.png'),
+  scienceInactive: require('../assets/icons/lib_science_inactive.png'),
+};
